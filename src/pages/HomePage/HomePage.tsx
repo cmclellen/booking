b68570b98fe674b85520ce6a622e1ddd8fea36bc
acmac <107990803+craig-mclellen-e20@users.noreply.github.com>
2023-12-29 06:14:52 +0800

    Initial styling

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { HomePageWrapper } from './HomePage.styled';
+
+interface HomePageProps {}
+
+const HomePage: FC<HomePageProps> = () => (
+ <HomePageWrapper>
+    HomePage Component
+ </HomePageWrapper>
+);
+
+export default HomePage;
