280b5e6849fbb43a0bd91743716270dd195bd712
acmac <craig.mclellen@eighty20solutions.com.au>
2023-12-29 12:35:18 +0800

    Fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,8 @@ import axios from 'axios';
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
+console.log(`base url: ${import.meta.env.VITE_API_BASE_URL}`);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter basename='/'>
