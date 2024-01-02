ed557b11716ee189394d5863514b89d4d856c726
acmac <craig.mclellen@eighty20solutions.com.au>
2024-01-02 17:46:52 +0800

    Fix

--- frontend/src/components/Header/Header.styled.ts
+++ frontend/src/components/Header/Header.styled.ts
@@ -2,3 +2,11 @@ import styled from 'styled-components';
 
 export const HeaderWrapper = styled.div`
 `;
+
+export const Brand = styled.a`
+   font-family: RubikDoodleShadow, serif;
+`;
+
+export const AppTitle = styled.small`
+   font-family: CarterOne, serif;
+`;
