b2d61b987d403ce45aea82275238e97bf4d2d806
cmclellen <cmclellen@gmail.com>
2024-01-02 17:37:21 +0800

    Add bootstrap js

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
-// import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 import { HashRouter } from 'react-router-dom';
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
@@ -11,7 +11,7 @@ console.log(`API base url: ${apiBaseUrl}`);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter basename='/'>
-      <App />      
+      <App />
     </HashRouter>
   </React.StrictMode>,
 )
