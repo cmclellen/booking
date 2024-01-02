d9be5580cf5e87f9274b093665fcd6937c6f4346
cmclellen <cmclellen@gmail.com>
2024-01-02 09:38:23 +0800

    Add compensating actions when failure occurs

--- backend/Reservations.Functions/Reservation.cs
+++ backend/Reservations.Functions/Reservation.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Globalization;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
@@ -12,10 +12,10 @@ using Microsoft.Extensions.Logging;
 
 namespace Reservations.Functions
 {
-    public static class Reservation
+    public class Reservation
     {
         [FunctionName("negotiate")]
-        public static SignalRConnectionInfo Negotiate(
+        public SignalRConnectionInfo Negotiate(
             [HttpTrigger(AuthorizationLevel.Anonymous)]
             HttpRequest req,
             [SignalRConnectionInfo(HubName = "serverless")]
@@ -25,82 +25,120 @@ namespace Reservations.Functions
         }
 
         [FunctionName(nameof(Reservation))]
-        public static async Task RunOrchestrator(
+        public async Task RunOrchestrator(
             [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
             Console.WriteLine(context.InstanceId);
             var makeReservationRequest = context.GetInput<ReservationRequest>();
 
-            await context.CallActivityAsync(nameof(ReserveFlight), makeReservationRequest.ConnectionId);
-            await context.CallActivityAsync(nameof(ReserveCar), makeReservationRequest.ConnectionId);
-            await context.CallActivityAsync(nameof(ReserveHotel), makeReservationRequest.ConnectionId);
+            await context.CallActivityAsync(nameof(ReserveFlight), makeReservationRequest);
+            try
+            {
+                await context.CallActivityAsync(nameof(ReserveCar), makeReservationRequest);
+
+                try
+                {
+                    await context.CallActivityAsync(nameof(ReserveHotel), makeReservationRequest);
+                }
+                catch (Exception)
+                {
+                    await context.CallActivityAsync(nameof(CancelCarReservation), makeReservationRequest);
+                    await context.CallActivityAsync(nameof(CancelFlightReservation), makeReservationRequest);
+                }
+            }
+            catch (Exception)
+            {
+                await context.CallActivityAsync(nameof(CancelFlightReservation), makeReservationRequest);
+            }
+        }
+
+        [FunctionName(nameof(CancelCarReservation))]
+        public async Task CancelCarReservation([ActivityTrigger] ReservationRequest reservationRequest, ILogger log,
+            [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages,
+            CancellationToken cancellationToken)
+        {
+            var type = "Car";
+            await CancelReservation(type, reservationRequest, signalRMessages, cancellationToken);
+        }
+
+        [FunctionName(nameof(CancelFlightReservation))]
+        public async Task CancelFlightReservation([ActivityTrigger] ReservationRequest reservationRequest, ILogger log,
+            [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages,
+            CancellationToken cancellationToken)
+        {
+            var type = "Flight";
+            await CancelReservation(type, reservationRequest, signalRMessages, cancellationToken);
+        }
+
+        private async Task CancelReservation(string type, ReservationRequest reservationRequest, IAsyncCollector<SignalRMessage> signalRMessages, CancellationToken cancellationToken)
+        {
+            var connectionId = reservationRequest.ConnectionId;
+            await SendMessageAsync(connectionId, signalRMessages, $"Cancelling {type} reservation...", cancellationToken);
+            await SimulateProcessRequest(type, reservationRequest, signalRMessages, false, cancellationToken);
+            await SendMessageAsync(connectionId, signalRMessages, $"{type} reservation cancelled.", cancellationToken);
         }
 
         [FunctionName(nameof(ReserveCar))]
-        public static async Task ReserveCar([ActivityTrigger] string connectionId, ILogger log,
-            [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages)
+        public async Task ReserveCar([ActivityTrigger] ReservationRequest reservationRequest, ILogger log,
+            [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages,
+            CancellationToken cancellationToken)
+        {
+            var type = "Car";
+            await MakeReservation(type, reservationRequest, signalRMessages, cancellationToken);
+        }
+
+        private async Task MakeReservation(string type, ReservationRequest reservationRequest, IAsyncCollector<SignalRMessage> signalRMessages, CancellationToken cancellationToken)
+        {
+            var connectionId = reservationRequest.ConnectionId;
+            await SendMessageAsync(connectionId, signalRMessages, $"Reserving {type}...", cancellationToken);
+            await SimulateProcessRequest(type, reservationRequest, signalRMessages, true, cancellationToken);
+            await SendMessageAsync(connectionId, signalRMessages, $"{type} reserved.", cancellationToken);
+        }
+
+        private async Task SendMessageAsync(string connectionId,
+            IAsyncCollector<SignalRMessage> signalRMessages,
+            string message,
+            CancellationToken cancellationToken)
         {
             await signalRMessages.AddAsync(new SignalRMessage
             {
                 ConnectionId = connectionId,
-                Target = "FlightBookedEvent",
-                Arguments = new[] { $"{DateTime.Now.ToString(CultureInfo.InvariantCulture)}; Booking car..." }
-            });
-
-            await Task.Delay(1000);
-
-            await signalRMessages.AddAsync(new SignalRMessage
-            {
-                ConnectionId = connectionId,
-                Target = "FlightBookedEvent",
-                Arguments = new[] { $"{DateTime.Now.ToString(CultureInfo.InvariantCulture)}; Car booked." }
-            });
+                Target = "ReservationEvent",
+                Arguments = new[] { message }
+            }, cancellationToken);
         }
 
         [FunctionName(nameof(ReserveHotel))]
-        public static async Task ReserveHotel([ActivityTrigger] string connectionId, ILogger log,
-            [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages)
+        public async Task ReserveHotel([ActivityTrigger] ReservationRequest reservationRequest, ILogger log,
+            [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages,
+            CancellationToken cancellationToken)
         {
-            await signalRMessages.AddAsync(new SignalRMessage
-            {
-                ConnectionId = connectionId,
-                Target = "FlightBookedEvent",
-                Arguments = new[] { $"{DateTime.Now.ToString(CultureInfo.InvariantCulture)}; Booking hotel..." }
-            });
-
-            await Task.Delay(1000);
-
-            await signalRMessages.AddAsync(new SignalRMessage
-            {
-                ConnectionId = connectionId,
-                Target = "FlightBookedEvent",
-                Arguments = new[] { $"{DateTime.Now.ToString(CultureInfo.InvariantCulture)}; Hotel booked." }
-            });
+            var type = "Hotel";
+            await MakeReservation(type, reservationRequest, signalRMessages, cancellationToken);
         }
 
         [FunctionName(nameof(ReserveFlight))]
-        public static async Task ReserveFlight([ActivityTrigger] string connectionId, ILogger log,
-            [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages)
+        public async Task ReserveFlight([ActivityTrigger] ReservationRequest reservationRequest, ILogger log,
+            [SignalR(HubName = "serverless")] IAsyncCollector<SignalRMessage> signalRMessages,
+            CancellationToken cancellationToken)
         {
-            await signalRMessages.AddAsync(new SignalRMessage
-            {
-                ConnectionId = connectionId,
-                Target = "FlightBookedEvent",
-                Arguments = new[] { $"{DateTime.Now.ToString(CultureInfo.InvariantCulture)}; Booking flight..." }
-            });
+            var type = "Flight"; await MakeReservation(type, reservationRequest, signalRMessages, cancellationToken);
+        }
 
-            await Task.Delay(1000);
-
-            await signalRMessages.AddAsync(new SignalRMessage
+        private async Task SimulateProcessRequest(string type, ReservationRequest reservationRequest, IAsyncCollector<SignalRMessage> signalRMessages, bool canFail,
+            CancellationToken cancellationToken = default)
+        {
+            await Task.Delay(1000, cancellationToken);
+            if (canFail && reservationRequest.SimulateFailure == type)
             {
-                ConnectionId = connectionId,
-                Target = "FlightBookedEvent",
-                Arguments = new[] { $"{DateTime.Now.ToString(CultureInfo.InvariantCulture)}; Flight booked." }
-            });
+                var connectionId = reservationRequest.ConnectionId;
+                await SendMessageAsync(connectionId, signalRMessages, $"Error occurred reserving {type}.", cancellationToken);
+                throw new Exception("Simulated error");
+            }
         }
 
         [FunctionName("Reservation_HttpStart")]
-        public static async Task<HttpResponseMessage> HttpStart(
+        public async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]
             HttpRequestMessage req,
             [DurableClient] IDurableOrchestrationClient starter,
@@ -108,9 +146,7 @@ namespace Reservations.Functions
         {
             var makeReservationRequest = await req.Content!.ReadFromJsonAsync<ReservationRequest>();
             var instanceId = await starter.StartNewAsync(nameof(Reservation), makeReservationRequest);
-
             log.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
-
             return starter.CreateCheckStatusResponse(req, instanceId);
         }
     }
