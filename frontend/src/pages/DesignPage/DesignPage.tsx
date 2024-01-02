124e3ca5c7aa6c64a1f9d968d425e46ba184dff2
acmac <craig.mclellen@eighty20solutions.com.au>
2024-01-02 16:43:01 +0800

    Fix

--- frontend/src/pages/DesignPage/DesignPage.tsx
+++ frontend/src/pages/DesignPage/DesignPage.tsx
@@ -18,12 +18,11 @@ const DesignPage: FC<DesignPageProps> = () => (
             The UI is a React SPA that interfaces with an Azure Function App. The user initiates a reservation via a button click on the UI, which calls through to an <i>Http</i> triggered Azure Function.
             This function then initates the orchestration by calling a Durable Function. All events throughout the orchestration are reported back to the UI via an Azure SignalR service that the Durable Function interacts with.
             The user has the option to simulate a failure at any one of the reservation steps, which would then demonstrate compensating actions cancelling any prior reservations, ensuring the reservation is not left in an inconsistent state.
-
-            <div className="alert alert-warning" role="alert">
-               <FontAwesomeIcon icon={faTriangleExclamation}></FontAwesomeIcon> In a real world scenario, other solutions could be incorporated to ensure the entire reservation eventually completes successfully without the reservation being
-               cancelled, eg. retries, etc. This example was just to demo sagas using Durable functions and compensating actions.
-            </div>
          </p>
+         <div className="alert alert-warning" role="alert">
+            <FontAwesomeIcon icon={faTriangleExclamation}></FontAwesomeIcon> In a real world scenario, other solutions could be incorporated to ensure the entire reservation eventually completes successfully without the reservation being
+            cancelled, eg. retries, etc. This example was just to demo sagas using Durable functions and compensating actions.
+         </div>
          <Heading>Architecture Diagram</Heading>
          <img src={designsvg} alt="Design" className="img-fluid" />
       </div>
