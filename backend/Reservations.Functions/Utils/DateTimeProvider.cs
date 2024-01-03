bccda14c599c270426cf60c4d86f87473ca4d2dd
acmac <craig.mclellen@eighty20solutions.com.au>
2024-01-04 07:12:45 +0800

    Fix

---
+++ backend/Reservations.Functions/Utils/DateTimeProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Reservations.Functions.Utils
+{
+    public interface IDateTimeProvider
+    {
+        DateTime Now { get; }
+        DateTime UtcNow { get; }
+    }
+
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        private static IDateTimeProvider _current;
+
+        private DateTimeProvider()
+        {
+        }
+
+        public static IDateTimeProvider Current
+        {
+            get => _current ?? new DateTimeProvider();
+            set => _current = value;
+        }
+
+        public DateTime Now => DateTime.Now;
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
